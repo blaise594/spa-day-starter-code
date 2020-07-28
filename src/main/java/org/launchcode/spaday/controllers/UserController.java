26c45ebbd20d66ac08117b6d46afd2cdaed22222
Daniel Rogers <danielbrogers594@gmail.com>
2020-07-28 19:37:20 -0400

    started studio 11

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,11 @@
+package org.launchcode.spaday.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//Add the @Controller annotation,
+@Controller
+//along with @RequestMapping("user") to configure routes into the controller.
+@RequestMapping("user")
+public class UserController {
+}
