e221d10eeec76cbe767cd46dd0ca8b6a52714fc5
Daniel Rogers <danielbrogers594@gmail.com>
2020-07-28 21:28:33 -0400

    finished basics of spa user signup studio

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //Add the @Controller annotation,
@@ -15,5 +19,21 @@ public class UserController {
         //This handler should correspond to the path /user/add, and for now,
         // it can just return the path to the add.html template.
         return "user/add";
+    }
+    //This will use model binding to create a new user object, user, and pass it into your handler method.
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        // add form submission handling code here
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if(user.getPassword().equals(verify)){
+            return  "user/index";
+        }
+        else {
+            model.addAttribute("error", "Invalid password");
+            return "user/add";
+        }
     }
 }
