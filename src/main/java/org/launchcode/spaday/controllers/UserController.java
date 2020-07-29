54e3870ae25b34d53581e97af47850e973975383
Daniel Rogers <danielbrogers594@gmail.com>
2020-07-28 20:32:14 -0400

    added user model,, controller and add.html form

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,6 +1,7 @@
 package org.launchcode.spaday.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //Add the @Controller annotation,
@@ -8,4 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //along with @RequestMapping("user") to configure routes into the controller.
 @RequestMapping("user")
 public class UserController {
+    //create a handler method displayAddUserForm() to render the form.
+    @GetMapping("/add")
+    public String displayAddUserForm(){
+        //This handler should correspond to the path /user/add, and for now,
+        // it can just return the path to the add.html template.
+        return "user/add";
+    }
 }
